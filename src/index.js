476ccfec5fcf4e1b6ff38c556661a537c5fe6d3b
Ed Melech <mr.melech@gmail.com>
2021-05-21 16:27:14 +0100

    refactor data loading to lifecycle  Methods

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null, errorMessage: '' };
+  }
 
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+
+  componentDidMount() {
+      window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
